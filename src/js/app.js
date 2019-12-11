851eaeae2f45eca1660b71e9f2c949f8c92e0108
Anna Sh <anna.sh.pb@gmail.com>
2019-12-12 00:45:23 +0200

    mod13plusbutton

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,43 @@ import searchService from './services/apiService';
 import { refs } from './utils/refs';
 import photoCardTemplate from './templates/photo-card.hbs';
 const debounce = require('lodash.debounce');
+import { pWarning, pNotice } from './utils/pnotify';
+import { messages } from './utils/messages';
 
 function searchFormSubmitHandler(event) {
   event.preventDefault();
   const form = event.target;
-  const inputValue = form.value;
+  const inputValue = form.value.toLowerCase();
   searchService.searchQuery = inputValue;
+  searchService
+    .fetchImages()
+    .then(data => {
+      if (data.hits.length === 0) {
+        pWarning(messages.warningNotIn);
+      } else if (inputValue.length === 0) {
+        clearListItems();
+      } else {
+        clearListItems();
+        const markup = buildPhotoCardMarkup(data);
+        insertPhotoCards(markup);
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+function loadMoreBtnHandler() {
   searchService.fetchImages().then(data => {
-     const resultArr = Array.from(data);
-      console.log(data);
-    const markup = buildPhotoCardMarkup(data);
-    console.log(markup);
-    insertPhotoCards(markup);
+    if (searchService.searchQuery.length === 0) {
+        pNotice(messages.warningNoInput);
+    } else {
+      const markup = buildPhotoCardMarkup(data);
+      insertPhotoCards(markup);
+      window.scrollBy({
+        top: window.innerHeight,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
   });
 }
 
@@ -25,7 +50,13 @@ function insertPhotoCards(items) {
   refs.imagesList.insertAdjacentHTML('beforeend', items);
 }
 
+function clearListItems() {
+  refs.imagesList.innerHTML = '';
+}
+
 refs.searchForm.addEventListener(
   'input',
   debounce(searchFormSubmitHandler, 500),
 );
+
+refs.loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
